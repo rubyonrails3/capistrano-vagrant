919346eab2111dd59777ed5b96681e773ecd38c7
Ismail Akram <rubyonrails3@gmail.com>
2015-02-15 22:44:53 +0500

    Added Something just

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid only for Capistrano 3.1
 lock '3.2.1'
 
-set :application, 'my_app_name'
+set :application, 'capistrano_project'
 set :repo_url, 'git@github.com:rubyonrails3/capistrano-vagrant.git'
 set :user, 'vagrant'
 
@@ -24,7 +24,7 @@ set :format, :pretty
 set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml}
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
